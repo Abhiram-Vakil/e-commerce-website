296700495309ebab48c54287672341e0bdbd02b0
Abhiram-Vakil <abhiramvakil@gmail.com>
2025-07-11 22:49:23 +0530

    created user and product model

--- ecommerce-server/src/server.js
+++ ecommerce-server/src/server.js
@@ -4,23 +4,21 @@ import cors from "cors";
 import authRoute from "./routes/authRoutes.js";
 import { connectDB } from "./config/db.js";
 
-
 dotenv.config();
 
 const PORT = process.env.PORT;
 const app = express();
-connectDB()
+connectDB();
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
-app.get("/",(req,res)=>{
-    res.send("Helloworld")
-})
+
+app.get("/", (req, res) => {
+  res.send("Helloworld");
+});
+
 app.listen(PORT, () => {
   console.log(
     `server is running at port ${PORT} url http://localhost:${PORT} `
   );
 });
-
-//
-
