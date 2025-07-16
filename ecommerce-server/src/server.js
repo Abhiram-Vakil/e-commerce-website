59d33ea22537a9d5a269d678c7cd99fae904a4db
Abhiram-Vakil <abhiramvakil@gmail.com>
2025-07-16 20:04:53 +0530

    updated server

--- ecommerce-server/src/server.js
+++ ecommerce-server/src/server.js
@@ -17,7 +17,7 @@ connectDB();
 app.use(express.json());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use("/api/auth", authRoutes);
-app.use("/api/product", productRoutes);
+app.use("/api", productRoutes);
 
 app.get("/", protect, (req, res) => {
   res.json({ message: "You are authorized", user: req.user });
