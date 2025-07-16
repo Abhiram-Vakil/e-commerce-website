6701142a41a22896193261cfb0b0a51ee16c1138
Abhiram-Vakil <abhiramvakil@gmail.com>
2025-07-16 20:11:58 +0530

    made product model

--- ecommerce-server/src/models/Product.js
+++ ecommerce-server/src/models/Product.js
@@ -2,12 +2,39 @@ import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema(
   {
-    name: String,
-    price: Number,
-    image: String,
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+  }
 );
 
-const Product = mongoose.model("Product",productSchema);
+const Product = mongoose.model("Product", productSchema);
 export default Product;
